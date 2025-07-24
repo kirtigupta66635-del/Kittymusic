bc35abc46ec8154789d87a71628210f84920ce48
VAMPIRE BOTS <vampirebots27@gmail.com>
2025-07-24 11:16:47 +0530

    Update __main__.py

--- AYESHA/__main__.py
+++ AYESHA/__main__.py
@@ -50,8 +50,7 @@ async def init():
         pass
     await Sona.decorators()
     LOGGER("AYESHA").info(
-        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @llVAMPIRE_KINGlll
-  , @lllVAMPIRE_UPDATElll FOR ANY ISSUES MADE BY AYESHA"
+        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @ll_ALPHA_BABY_lll , @PURVI_SUPPORT FOR ANY ISSUES MADE BY AYESHA"
     )
     await idle()
     await app.stop()
