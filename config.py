db9b86594f03a49df296053b8b519016695c3ad2
VAMPIRE BOTS <vampirebots27@gmail.com>
2025-07-24 10:24:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN",  "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lllVAMPIRE_UPDATElll")
