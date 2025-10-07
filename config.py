4559d202bbd4520285407b9f2d5cb6ca3743d6af
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-10-07 17:21:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vampirebotsntwrk/Kittymusic",
+    "https://github.com/kirtigupta66635-del/Kittymusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
